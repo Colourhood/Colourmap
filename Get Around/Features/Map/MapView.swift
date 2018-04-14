051c8ef6fe559835ae5896ebff8ba71662c99eab
Andrei <andrei.villasana@smashingideas.com>
2018-04-14 01:24:03 -0700

    refactor

--- Get Around/Features/Map/MapView.swift
+++ Get Around/Features/Map/MapView.swift
@@ -13,9 +13,10 @@ class MapView: UIViewController {
 extension MapView {
 
     private func renderCallOut() {
-        componentSubview.frame.size = CGSize(width: view.frame.width * 0.7, height: view.frame.height * 0.07)
-        componentSubview.center.y = Positions.Vertical.center
-        componentSubview.center.x = Positions.Horizontal.center
+        componentSubview.frame.size = CGSize(width: view.frame.width * 0.7,
+                                             height: view.frame.height * 0.07)
+        componentSubview.center.y = Positions.centerY
+        componentSubview.center.x = Positions.centerX
         componentSubview.alpha = 0
 
         guard let callOut: CallOut = renderNib() else { return }
@@ -44,8 +45,19 @@ extension MapView {
             self.componentSubview.frame.size = CGSize(width: UIScreen.main.bounds.width,
                                                       height: self.view.frame.height * 0.13)
             self.componentSubview.frame.origin = CGPoint(x: 0, y: 20)
+        }, completion: { _ in
+            self.renderPin()
         })
+    }
 
+    private func renderPin() {
+        guard let pin: Pin = renderNib() else { return }
+        pin.frame = CGRect(x: Positions.centerX - 20,
+                           y: Positions.centerY - 20,
+                           width: view.bounds.width / 10,
+                           height: view.bounds.width / 10)
+
+        view.addSubview(pin)
     }
 }
 
--- Get Around/Layout/Positions.swift
+++ Get Around/Layout/Positions.swift
@@ -1,14 +1,6 @@
 import UIKit
 
 struct Positions {
-    struct Vertical {
-        static let center: CGFloat = UIScreen.main.bounds.midY
-        static func bottom(view: UIView) -> CGFloat {
-            return UIScreen.main.bounds.height - view.frame.height - 40
-        }
-    }
-
-    struct Horizontal {
-        static let center: CGFloat = UIScreen.main.bounds.midX
-    }
+    static let centerY: CGFloat = UIScreen.main.bounds.midY
+    static let centerX: CGFloat = UIScreen.main.bounds.midX
 }

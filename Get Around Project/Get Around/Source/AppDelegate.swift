ff8d6ae2f2344931ef83c936d4205b36e18b4ca8
Andrei <andreicrimson@gmail.com>
2018-06-28 21:40:50 -0700

    programatically adding view controller to navigation controller hierarchy

--- Get Around Project/Get Around/Source/AppDelegate.swift
+++ Get Around Project/Get Around/Source/AppDelegate.swift
@@ -6,6 +6,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainController())
+        navigationController.isNavigationBarHidden = true
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 }
